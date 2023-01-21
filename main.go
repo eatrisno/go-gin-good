d0a58a046621714a85942e442e4ebdf7dddb68f1
eko <eko@investree.id>
2023-01-21 18:37:49 +0700

    optimize

--- main.go
+++ main.go
@@ -18,27 +18,20 @@ import (
 func init() {
 	setting.Setup()
 	logging.Log.Info().Msg("App is starting...")
-
 }
+
 func main() {
 	routersInit := routers.InitRouter()
-
-	readTimeout := setting.ServerSetting.ReadTimeout
-	writeTimeout := setting.ServerSetting.WriteTimeout
-	idleTimeout := setting.ServerSetting.IdleTimeout
-	readHeaderTimeout := setting.ServerSetting.ReadHeaderTimeout
-
 	endPoint := fmt.Sprintf("%s:%d", setting.ServerSetting.HttpHost, setting.ServerSetting.HttpPort)
-	maxHeaderBytes := 1 << 20
 
 	server := &http.Server{
 		Addr:              endPoint,
 		Handler:           routersInit,
-		ReadTimeout:       readTimeout,
-		WriteTimeout:      writeTimeout,
-		IdleTimeout:       idleTimeout,
-		ReadHeaderTimeout: readHeaderTimeout,
-		MaxHeaderBytes:    maxHeaderBytes,
+		ReadTimeout:       setting.ServerSetting.ReadTimeout,
+		WriteTimeout:      setting.ServerSetting.WriteTimeout,
+		IdleTimeout:       setting.ServerSetting.IdleTimeout,
+		ReadHeaderTimeout: setting.ServerSetting.ReadHeaderTimeout,
+		MaxHeaderBytes:    1 << 20,
 	}
 
 	logging.Log.Info().Msg("Server is running...")
@@ -46,15 +39,16 @@ func main() {
 	var gracefulStop = make(chan os.Signal)
 	signal.Notify(gracefulStop, syscall.SIGTERM)
 	signal.Notify(gracefulStop, syscall.SIGINT)
+
 	go func() {
 		sig := <-gracefulStop
 		logging.Log.Info().Msgf("caught sig: %+v", sig)
-		logging.Log.Info().Msg("Wait for 3 second to finish processing")
-		time.Sleep(3 * time.Second)
-		if err := server.Shutdown(context.Background()); err != nil {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
 			logging.Log.Error().Msgf("Server Shutdown Failed:%+v", err)
 		}
-		logging.Log.Info().Msg("Server exiting")
 	}()
 
 	if err := server.ListenAndServe(); err != nil {
