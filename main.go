e62e8c732cea839da2c35fa7951773ca85567e1e
eko <eko@investree.id>
2023-01-21 16:36:35 +0700

    optimize

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/eatrisno/go-gin-good/resources/logging"
 	"github.com/eatrisno/go-gin-good/resources/setting"
@@ -38,8 +43,25 @@ func main() {
 
 	logging.Log.Info().Msg("Server is running...")
 
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
-	}
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+	go func() {
+		sig := <-gracefulStop
+		logging.Log.Info().Msgf("caught sig: %+v", sig)
+		logging.Log.Info().Msg("Wait for 3 second to finish processing")
+		time.Sleep(3 * time.Second)
+		if err := server.Shutdown(context.Background()); err != nil {
+			logging.Log.Error().Msgf("Server Shutdown Failed:%+v", err)
+		}
+		logging.Log.Info().Msg("Server exiting")
+	}()
 
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			logging.Log.Info().Msg("Server closed under request")
+		} else {
+			logging.Log.Error().Msgf("Server closed unexpected %+v", err)
+		}
+	}
 }
